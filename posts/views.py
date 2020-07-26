4bd0c292f49158c0cb0360a71e7afc05a0314a4b
Nikita Ustiuzhanin <zaebumba@gmail.com>
2020-07-26 17:12:19 +0700

    Fixing new_post

--- posts/views.py
+++ posts/views.py
@@ -18,10 +18,10 @@ def group_posts(request, slug):
 
 @login_required
 def new_post(request):
-    if request.method != "POST":
-        form = PostForm()
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
 
-    form = PostForm(request.POST)
     if form.is_valid():
         post = form.save(commit=False)
         post.author = request.user
